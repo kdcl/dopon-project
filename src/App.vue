247c24da9da26ed769e6c30b4940cf5f5a6437b3
Angela <angela@AngeladeMacBook-Air.local>
2021-07-10 16:28:02 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <div>kdcl22</div>
+    <div>kdcl2233333</div>
   </div>
   <router-view/>
   <button type="button" class="btn btn-primary">Primary</button>
